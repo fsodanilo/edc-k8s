85c9432797a7aab2d722c632e0c0e48e3b8d2b41
Danilotve <fsodanilo@gmail.com>
2021-09-30 17:54:46 -0300

    Deploy airflow enade

---
+++ dags/enade_spark_k8s_processing.py
@@ -0,0 +1,73 @@
+from airflow import DAG
+
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+import boto3
+
+aws_access_key_id = Variable.get('aws_access_key_id')
+aws_secret_access_key = Variable.get('aws_secret_access_key')
+glue = boto3.client('glue', region_name='us-east-2',
+                    aws_access_key_id=aws_access_key_id, 
+                    aws_secret_access_key=aws_secret_access_key)
+
+from airflow.utils.dates import days_ago
+
+def trigger_crawler_final_func():
+        glue.start_crawler(Name='enade2017')
+
+
+with DAG(
+    'enade_batch_spark_k8s',
+    default_args={
+        'owner': 'Danilo',
+        'depends_on_past': False,
+        'email': ['fsodanilo@gmail.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'max_active_runs': 1,
+    },
+    description='submit spark-pi as sparkApplication on kubernetes',
+    schedule_interval="0 */2 * * *",
+    start_date=days_ago(1),
+    catchup=False,
+    tags=['spark', 'kubernetes', 'batch', 'enem'],
+) as dag:
+    converte_parquet = SparkKubernetesOperator(
+        task_id='converte_parquet',
+        namespace="airflow",
+        application_file="enade_converte_parquet.yaml",
+        kubernetes_conn_id="kubernetes_default",
+        do_xcom_push=True,
+    )
+
+    converte_parquet_monitor = SparkKubernetesSensor(
+        task_id='converte_parquet_monitor',
+        namespace="airflow",
+        application_name="{{ task_instance.xcom_pull(task_ids='converte_parquet')['metadata']['name'] }}",
+        kubernetes_conn_id="kubernetes_default",
+    )
+
+    converte_nota = SparkKubernetesOperator(
+        task_id='converte_nota',
+        namespace="airflow",
+        application_file="enade_converte_nota.yaml",
+        kubernetes_conn_id="kubernetes_default",
+        do_xcom_push=True,
+    )
+
+    converte_nota_monitor = SparkKubernetesSensor(
+        task_id='converte_nota_monitor',
+        namespace="airflow",
+        application_name="{{ task_instance.xcom_pull(task_ids='converte_nota')['metadata']['name'] }}",
+        kubernetes_conn_id="kubernetes_default",
+    )
+
+    trigger_crawler_final = PythonOperator(
+        task_id='trigger_crawler_final',
+        python_callable=trigger_crawler_final_func,
+    )
+
+converte_parquet >> converte_parquet_monitor >> converte_nota >> converte_nota_monitor
+trigger_crawler_final
---
+++ dags/pyspark/enade_converte_parquet.py
@@ -0,0 +1,51 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+
+# set conf
+conf = (
+SparkConf()
+    .set("spark.hadoop.fs.s3a.fast.upload", True)
+    .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    .set('spark.hadoop.fs.s3a.aws.credentials.provider', 'com.amazonaws.auth.EnvironmentVariableCredentialsProvider')
+    .set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.3')
+)
+
+# apply config
+sc = SparkContext(conf=conf).getOrCreate()
+    
+
+if __name__ == "__main__":
+
+    # init spark session
+    spark = SparkSession\
+            .builder\
+            .appName("ENADE Job")\
+            .getOrCreate()
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    df = (
+        spark
+        .read
+        .option("header", True)
+        .option("delimiter", ";")
+        .option("inferSchema", True)
+        .csv("s3a://datalake-brx-edc/raw-data/enade/enade2017.txt")
+    )
+    
+    df.printSchema()
+
+    (
+        df
+        .write
+        .mode("overwrite")
+        .format("parquet")
+        .save("s3a://datalake-brx-edc/staging/enade")    
+    )
+
+
+    print("*********************")
+    print("Escrito com sucesso!")
+    print("*********************")
+
+    spark.stop()
