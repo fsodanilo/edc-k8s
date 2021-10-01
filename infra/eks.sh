5f960752196bd73f242469596e2b9efcd28969c4
Danilotve <fsodanilo@gmail.com>
2021-10-01 10:38:52 -0300

    move job_clue

--- infra/eks.sh
+++ infra/eks.sh
@@ -1,3 +1,3 @@
 eksctl create cluster --name=edck8s --managed --spot --instance-types=m5.xlarge \
-    --nodes=2 --alb-ingress-access --node-private-networking --region=us-east-2 \
-    --nodes-min=2 --nodes-max=3 --full-ecr-access --asg-access --nodegroup-name=ng-edck8s
+    --nodes=5 --alb-ingress-access --node-private-networking --region=us-east-2 \
+    --nodes-min=3 --nodes-max=6 --full-ecr-access --asg-access --nodegroup-name=ng-edck8s
